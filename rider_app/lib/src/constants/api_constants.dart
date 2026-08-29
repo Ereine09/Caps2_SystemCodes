646076abca3532a3c6ca8f29de486d01682f4d65
ereine <ereine09@gmail.com>
2026-08-29 18:12:35 +0800

    few debug

--- rider_app/lib/src/constants/api_constants.dart
+++ rider_app/lib/src/constants/api_constants.dart
@@ -11,7 +11,7 @@ const String _physicalDeviceBaseUrl = 'http://192.168.1.84/loyalty_managements';
 // --- SELECT YOUR MOBILE TARGET ENVIRONMENT ---
 // Use _emulatorBaseUrl for the Android emulator.
 // Use _physicalDeviceBaseUrl for a real phone connected via Wi-Fi.
-const String _mobileBaseUrl = _emulatorBaseUrl;
+const String _mobileBaseUrl = _physicalDeviceBaseUrl;
 
 // This is the final URL the app will use. No need to change this line.
 const String baseUrl = kIsWeb ? _webBaseUrl : _mobileBaseUrl;
